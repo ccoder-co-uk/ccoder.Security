3fedd6c36458dbb8ed389139103048cc6c02d9d5
Paul Ward <paul.ward@corporatelinx.com>
2024-05-02 10:30:31 +0100

    FOUNDATION: Add Tenant Analysis Service

---
+++ src/cCoder.Security.Services/Foundation/Interfaces/ITenantAnalysisService.cs
@@ -0,0 +1,10 @@
+﻿using cCoder.Security.Objects.Entities;
+
+namespace cCoder.Security.Services.Foundation.Interfaces;
+public interface ITenantAnalysisService
+{
+    ValueTask<TenantAnalysis> AddTenantAnalaysisAsync(TenantAnalysis tenant);
+    ValueTask DeleteTenantAnalysisAsync(TenantAnalysis tenant);
+    IQueryable<TenantAnalysis> GetAllTenantAnalysis();
+    ValueTask<TenantAnalysis> UpdateTenantAnalysisAsync(TenantAnalysis tenant);
+}
---
+++ src/cCoder.Security.Services/Foundation/TenantAnalysisService.cs
@@ -0,0 +1,24 @@
+﻿using cCoder.Security.Data.Brokers.Storage.Interfaces;
+using cCoder.Security.Objects.Entities;
+using cCoder.Security.Services.Foundation.Interfaces;
+
+namespace cCoder.Security.Services.Foundation;
+
+public class TenantAnalysisService(ITenantAnalysisBroker broker) 
+    : ITenantAnalysisService
+{
+    public async ValueTask<TenantAnalysis> AddTenantAnalaysisAsync(TenantAnalysis tenant)
+    {
+        tenant.CreatedOn = DateTimeOffset.UtcNow;
+        return await broker.AddTenantAnalysisAsync(tenant);
+    }
+
+    public async ValueTask DeleteTenantAnalysisAsync(TenantAnalysis tenant)
+        => await broker.DeleteTenantAnalysisAsync(tenant);
+
+    public IQueryable<TenantAnalysis> GetAllTenantAnalysis()
+        => broker.GetAllTenantAnalysis();
+
+    public async ValueTask<TenantAnalysis> UpdateTenantAnalysisAsync(TenantAnalysis tenant)
+        => await broker.UpdateTenantAnalysisAsync(tenant);
+}
