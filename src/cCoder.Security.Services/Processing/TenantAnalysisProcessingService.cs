216467f1159789042555c6de36e723c8623a0507
Paul Ward <paul.ward@corporatelinx.com>
2024-05-02 10:30:52 +0100

    PROCESSING: Add Tenant Analysis Processing Service

---
+++ src/cCoder.Security.Services/Processing/TenantAnalysisProcessingService.cs
@@ -0,0 +1,21 @@
+﻿using cCoder.Security.Objects.Entities;
+using cCoder.Security.Services.Foundation.Interfaces;
+
+namespace cCoder.Security.Services.Processing;
+
+public class TenantAnalysisProcessingService(ITenantAnalysisService tenantAnalysisService) 
+    : ITenantAnalysisProcessingService
+
+{
+    public ValueTask<TenantAnalysis> AddTenantAnalysisAsync(TenantAnalysis item) =>
+        tenantAnalysisService.AddTenantAnalaysisAsync(item);
+
+    public ValueTask DeleteTenantAnalysisAsync(TenantAnalysis item) =>
+        tenantAnalysisService.DeleteTenantAnalysisAsync(item);
+
+    public IQueryable<TenantAnalysis> GetAllTenantAnalysis() =>
+        tenantAnalysisService.GetAllTenantAnalysis();
+
+    public ValueTask<TenantAnalysis> UpdateTenantAnalysisAsync(TenantAnalysis item) =>
+        tenantAnalysisService.UpdateTenantAnalysisAsync(item);
+}
