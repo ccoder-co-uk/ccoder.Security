460497d960fdd6e2e60d21d3aa6ca09e4ed87f64
Paul Ward <paul.ward@corporatelinx.com>
2024-06-19 16:07:08 +0100

    MINOR FIX: Add Missing Controllers

--- src/cCoder.Security.Services/Processing/Interfaces/ISSOUserRoleProcessingService.cs
+++ src/cCoder.Security.Services/Processing/Interfaces/ISSOUserRoleProcessingService.cs
@@ -4,6 +4,7 @@ namespace cCoder.Security.Services.Processing.Interfaces;
 
 public interface ISSOUserRoleProcessingService
 {
+    IQueryable<SSOUserRole> GetAllSSOUserRoles();
     ValueTask<SSOUserRole> AddSSOUserRoleAsync(SSOUserRole item);
     ValueTask DeleteSSOUserRoleAsync(SSOUserRole item);
 }
