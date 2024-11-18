a746fcf1a74ddc25bc206ea352a28395a0f2ae78
Paul Ward <paul.ward@corporatelinx.com>
2024-11-18 18:41:01 +0000

    MINOR FIX: Move configuration to it's proper place in the DB Server specific libraries

---
+++ src/cCoder.Security.Data.EF.MSSQL/MSSQLSecurityDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using cCoder.Security.Data.EF.Interfaces;
+using cCoder.Security.Data.EF.MSSQL;
+using cCoder.Security.Objects;
+
+namespace cCoder.Security.Data.EF;
+
+// This Wrapper in case we want to take a different approach to the EF factory for our context instancing 
+// or just want a single place to call the ctor to avoid affecting a lot of files when the ctor changes.
+public class MSSQLSecurityDbContextFactory
+    : ISecurityDbContextFactory
+{
+    public Func<ISSOAuthInfo> GetAuthInfo { get; set; } =
+        () => new SSOAuthInfo { SSOUserId = "Guest" };
+
+    public SecurityDbContext CreateDbContext()
+    {
+        Console.WriteLine("Using MSSQLSecurityDbContextFactory to create DbContext");
+        return new(GetAuthInfo(), new SecurityMSSQLModelBuildProvider("SSO"));
+    }
+
+    public SecurityDbContext CreateDbContext(string[] args) =>
+        CreateDbContext();
+}
