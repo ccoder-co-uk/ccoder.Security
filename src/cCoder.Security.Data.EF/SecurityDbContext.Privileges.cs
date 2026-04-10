34509b3dde20da9564a7448ee544a7e6d0f742d6
ashley.upson <ashley@aupson.co.uk>
2026-04-10 15:51:01 +0100

    Minor feature: add security_admin priv to privs list and checks

--- src/cCoder.Security.Data.EF/SecurityDbContext.Privileges.cs
+++ src/cCoder.Security.Data.EF/SecurityDbContext.Privileges.cs
@@ -6,9 +6,14 @@ namespace cCoder.Security.Data.EF;
 public partial class SecurityDbContext : DbContext
 {
     readonly IEnumerable<SSOPrivilege> privileges = [
-        new () { Id = "tenant_create", Description = $"Allows users to Create Tenants." },
-        new () { Id = "tenant_Read", Description = $"Allows users to Read a Tenants." },
-        new() { Id = "tenant_admin", Description = $"Allows users to Administer a Tenant." }
+        new() { Id = "security_admin", Description = "Allows users to use or add privs they don't already have." },
+        new() { Id = "tenant_create", Description = "Allows users to Create Tenants." },
+        new() { Id = "tenant_Read", Description = "Allows users to Read a Tenants." },
+        new() { Id = "tenant_admin", Description = "Allows users to Administer a Tenant." },
+        new() { Id = "tenantsecret_create", Description = "Allows users to Create Tenant Secrets." },
+        new() { Id = "tenantsecret_read", Description = "Allows users to Read Tenant Secrets." },
+        new() { Id = "tenantsecret_update", Description = "Allows users to Update Tenant Secrets." },
+        new() { Id = "tenantsecret_delete", Description = "Allows users to Delete Tenant Secrets." }
     ];
 
     public IEnumerable<SSOPrivilege> GetPrivileges() => privileges;
