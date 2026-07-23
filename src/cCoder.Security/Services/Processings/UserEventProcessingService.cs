bea6170cfe5ecc2d36538429222b1ae40cc84668
TehWardy <paul.ward@ccoder.co.uk>
2026-07-23 19:47:08 +0100

    Apply tenant and user event processing safeguards

--- src/cCoder.Security/Services/Processings/UserEventProcessingService.cs
+++ src/cCoder.Security/Services/Processings/UserEventProcessingService.cs
@@ -8,15 +8,30 @@ using cCoder.Security.Services.Processings.Interfaces;
 
 namespace cCoder.Security.Services.Processings;
 
-internal class UserEventProcessingService(IUserEventService userEventService)
+internal sealed partial class UserEventProcessingService(IUserEventService userEventService)
         : IUserEventProcessingService
 {
     public ValueTask<UserEvent> AddUserEventAsync(UserEvent newUserEvent) =>
-        userEventService.AddUserEventAsync(userEvent: newUserEvent);
+        TryCatch<UserEvent>(operation: async () =>
+        {
+            ValidateUserEventOnAdd(newUserEvent: newUserEvent);
+
+            return await userEventService.AddUserEventAsync(userEvent: newUserEvent);
+        });
 
     public ValueTask DeleteUserEventAsync(UserEvent deletedUserEvent) =>
-        userEventService.DeleteUserEventAsync(userEvent: deletedUserEvent);
+        TryCatch(operation: async () =>
+        {
+            ValidateUserEventOnDelete(deletedUserEvent: deletedUserEvent);
+
+            await userEventService.DeleteUserEventAsync(userEvent: deletedUserEvent);
+        });
 
     public IQueryable<UserEvent> GetAllUserEvents() =>
-        userEventService.GetAllUserEvents();
+        TryCatch(operation: () =>
+        {
+            ValidateUserEventsOnGet();
+
+            return userEventService.GetAllUserEvents();
+        });
 }
