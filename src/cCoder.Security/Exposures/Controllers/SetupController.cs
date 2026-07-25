67a987cc3f70ec10feb496aaadcadc2d9c68e714
Paul Ward <paul.ward@ccoder.co.uk>
2026-07-25 20:04:59 +0100

    Add guarded Default tenant bootstrap endpoint (#86)
    * Add guarded default tenant setup endpoint
    
    * Comply with setup parameter naming

---
+++ src/cCoder.Security/Exposures/Controllers/SetupController.cs
@@ -0,0 +1,46 @@
+// ---------------------------------------------------------------
+// Copyright (c) Paul.Ward@ccoder.co.uk
+// ---------------------------------------------------------------
+
+using cCoder.Security.Data.Models;
+using cCoder.Security.Exposures;
+using cCoder.Security.Objects.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cCoder.Security.Exposures.Controllers;
+
+[Route("Api/Setup")]
+public sealed class SetupController(ITenantManager tenantManager)
+    : Controller
+{
+    [HttpPost]
+    public async ValueTask<IActionResult> PostSetup(
+        [FromBody] SetupDetails newSetupDetails)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(modelState: ModelState);
+        }
+
+        try
+        {
+            await tenantManager.SetupAsync(setupDetails: newSetupDetails);
+        }
+        catch (SecurityAggregationValidationException exception)
+        {
+            return Conflict(error: exception.InnerException?.Message);
+        }
+        catch (SecurityAggregationDependencyException)
+        {
+            return Problem(
+                statusCode: StatusCodes.Status503ServiceUnavailable);
+        }
+        catch (SecurityAggregationServiceException)
+        {
+            return Problem(
+                statusCode: StatusCodes.Status500InternalServerError);
+        }
+
+        return Ok();
+    }
+}
