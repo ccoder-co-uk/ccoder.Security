5d00ca119d18b33ed53abb30e6c228d2e50a96b4
TehWardy <paul.ward@ccoder.co.uk>
2026-08-03 06:40:33 +0100

    Accept workflow execution bearer tokens

--- src/cCoder.Security.Tests/Aggregations/SSOAuthInfoAggregationServiceTests.GetSSOAuthInfo.cs
+++ src/cCoder.Security.Tests/Aggregations/SSOAuthInfoAggregationServiceTests.GetSSOAuthInfo.cs
@@ -3,6 +3,7 @@
 // ---------------------------------------------------------------
 
 using cCoder.Security.Models.Configurations;
+using cCoder.Security.Models.Entities;
 using cCoder.Security.Services.Aggregations;
 using cCoder.Security.Services.Processings.Interfaces;
 using FluentAssertions;
@@ -13,6 +14,66 @@ namespace cCoder.Security.Tests.Aggregations;
 
 public sealed partial class SSOAuthInfoAggregationServiceTests
 {
+    [Theory]
+    [InlineData(TokenUse.Auth)]
+    [InlineData(TokenUse.WorkflowExecution)]
+    public async Task SupportedBearerAuthenticationReturnsTokenUser(
+        TokenUse tokenUse)
+    {
+        // Given
+        const string TokenId = "supported-token";
+        const string UserId = "authenticated-user";
+
+        Mock<ISessionProcessingService> sessionProcessingServiceMock =
+            new(MockBehavior.Strict);
+
+        Mock<ISSOUserProcessingService> ssoUserProcessingServiceMock =
+            new(MockBehavior.Strict);
+
+        Mock<ITokenProcessingService> tokenProcessingServiceMock =
+            new(MockBehavior.Strict);
+
+        Mock<IRequestProcessingService> requestProcessingServiceMock =
+            new(MockBehavior.Strict);
+
+        requestProcessingServiceMock
+            .Setup(expression: service =>
+                service.GetHeader(key: "Authorization"))
+            .Returns(value: $"Bearer {TokenId}");
+
+        tokenProcessingServiceMock
+            .Setup(expression: service =>
+                service.GetTokenById(tokenId: TokenId))
+            .Returns(value: new Token
+            {
+                Id = TokenId,
+                Reason = (int)tokenUse,
+                UserName = UserId
+            });
+
+        SSOAuthInfoAggregationService service = new(
+            sessionService: sessionProcessingServiceMock.Object,
+            userService: ssoUserProcessingServiceMock.Object,
+            tokenService: tokenProcessingServiceMock.Object,
+            requestProcessingService:
+                requestProcessingServiceMock.Object);
+
+        // When
+        ISSOAuthInfo authInfo = await service.GetSSOAuthInfoAsync();
+
+        // Then
+        authInfo.AuthenticationFailed.Should()
+            .BeFalse();
+
+        authInfo.SSOUserId.Should()
+            .Be(expected: UserId);
+
+        sessionProcessingServiceMock.VerifyNoOtherCalls();
+        ssoUserProcessingServiceMock.VerifyNoOtherCalls();
+        tokenProcessingServiceMock.VerifyAll();
+        requestProcessingServiceMock.VerifyAll();
+    }
+
     [Fact]
     public async Task InvalidBearerAuthenticationDoesNotFallBackToSession()
     {
