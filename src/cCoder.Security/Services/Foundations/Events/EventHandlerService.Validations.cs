3d733ea0489bfa9e921fc18f4d9ac7e4bb97cb16
TehWardy <paul.ward@ccoder.co.uk>
2026-08-12 14:06:59 +0100

    test: raise scoped coverage gate above 95 percent

--- src/cCoder.Security/Services/Foundations/Events/EventHandlerService.Validations.cs
+++ src/cCoder.Security/Services/Foundations/Events/EventHandlerService.Validations.cs
@@ -2,16 +2,6 @@
 // Copyright (c) Paul.Ward@ccoder.co.uk
 // ---------------------------------------------------------------
 
-
 namespace cCoder.Security.Services.Foundations.Events;
 
-internal sealed partial class EventHandlerService
-{
-    private static void Validate(params object[] inputs)
-    {
-        if (inputs.Any(predicate: input => input is null))
-        {
-            throw new ArgumentNullException(nameof(inputs));
-        }
-    }
-}
+internal sealed partial class EventHandlerService;
