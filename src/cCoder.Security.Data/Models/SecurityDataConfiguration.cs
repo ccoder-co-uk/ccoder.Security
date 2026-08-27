3ad0662c4fb4de8c6ac63aefaa2fec74d6455b18
TehWardy <paul.ward@ccoder.co.uk>
2026-08-27 12:01:18 +0100

    Add SecurityData configuration contract

---
+++ src/cCoder.Security.Data/Models/SecurityDataConfiguration.cs
@@ -0,0 +1,12 @@
+// ---------------------------------------------------------------
+// Copyright (c) Paul.Ward@ccoder.co.uk
+// ---------------------------------------------------------------
+
+namespace cCoder.Security.Models;
+
+public sealed class SecurityDataConfiguration
+{
+    public string ConnectionString { get; set; } = string.Empty;
+
+    public string AdminConnectionString { get; set; } = string.Empty;
+}
